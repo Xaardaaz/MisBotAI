4a65a3d3e78effc2498e22e08bf300e42ddc58b9
Xaardaaz <52383550+Xaardaaz@users.noreply.github.com>
2025-09-24 01:54:18 +0200

    Dodano zdarzenie streamNotification.ts

---
+++ src/events/streamNotification.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'streamNotification',
+  async execute(client: any) {
+    // Tutaj dodaj logikę sprawdzania transmisji (np. co 5 minut)
+    console.log('Sprawdzanie transmisji...');
+  },
+};
